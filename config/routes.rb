f62c9d2b9ce79427ed49ea51cb0eedc783ff9859
Michael Lannin <mlannin23@gmail.com>
2013-12-26 04:14:29 -0800

    Set up preliminary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ CodeTheChange::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'pages#home'
+
+  get '/about', to: 'pages#about'
+  get '/projects', to: 'pages#projects'
+
   namespace :admin do
     resources :events, :officers, :projects
   end
