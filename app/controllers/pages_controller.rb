bce045c28881e6db0fcd8a7ba04d0666b1f405d5
Michael Lannin <mlannin23@gmail.com>
2013-12-31 16:45:23 -0600

    Create Events section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
 
   def home
+    @events = Event.all
+    @upcoming_events = []
+    @past_events = []
+    time = Time.now
+
+    @events.each do |event|
+      if event.end > time
+        @upcoming_events.push(event)
+      else
+        @past_events.push(event)
+      end
+    end
   end
 
   def about
