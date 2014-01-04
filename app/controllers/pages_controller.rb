74c0e3786556db7226fea2ec51aa82238e347bab
Michael Lannin <mlannin23@gmail.com>
2014-01-03 17:30:40 -0800

    Sort events by date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @events = Event.all
+    @events.sort! { |a,b| b.start <=> a.start }
     @upcoming_events = []
     @past_events = []
     time = Time.now
