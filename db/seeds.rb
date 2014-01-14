da634fddd980c0b4a2b392ae7e835b8a5ba87096
Michael Lannin <mlannin23@gmail.com>
2014-01-14 01:36:22 -0800

    Events seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ events = [
   [ "Fall 2013 Infosession", DateTime.new(2013, 9, 10, 19, 0, 0, "-8"), DateTime.new(2013, 9, 10, 21, 0, 0, "-8"), "220 Wheeler, 122 Wheeler", "We will be holding two infosessions to kick off the Fall 2013 Semester. We will go over the club's mission and purpose, introduce our current officers, go over the projects we will be working on this semester, and give members the chance to sign up to participate!", 'https://www.facebook.com/events/518426954893420/' ]
 ]
 
-events.each do |title, start, end_time, location, description, url|
-  Event.create(:title => title, :start => start, :end => end_time, :location => location, :description => description, :url => url)
+events.each do |title, start_time, end_time, location, description, url|
+  Event.create(:title => title, :start_time => start_time, :end_time => end_time, :location => location, :description => description, :url => url)
 end
