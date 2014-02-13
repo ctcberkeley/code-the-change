8ea32ad8b9ce698f691e7c368c59fa28ca14ec7d
Michael Lannin <mlannin23@gmail.com>
2014-02-13 01:04:31 -0800

    Create paths for logging in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CodeTheChange::Application.routes.draw do
-  devise_for :admins
+  devise_for :admin, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
