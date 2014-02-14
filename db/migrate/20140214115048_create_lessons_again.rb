0da57ca7ef31529f640f76186e960afaef6fb291
Michael Lannin <mlannin23@gmail.com>
2014-02-14 03:52:15 -0800

    Trying to fix database problem

---
+++ db/migrate/20140214115048_create_lessons_again.rb
@@ -0,0 +1,12 @@
+class CreateLessonsAgain < ActiveRecord::Migration
+  def change
+    create_table :lessons do |t|
+      t.integer :week
+      t.date :date
+      t.string :topic
+      t.string :slides
+
+      t.timestamps
+    end
+  end
+end
