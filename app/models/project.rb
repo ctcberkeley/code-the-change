af5a8b52e61eddaad4b85526794fc7f3c5bb6fe1
Michael Lannin <mlannin23@gmail.com>
2014-03-27 21:19:45 -0700

    Change project image size

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
   validates :description, presence: true
 
   has_attached_file :image, 
-    :styles => { :thumbnail => "120x90>", :medium => "768x576>" }, 
+    :styles => { :thumbnail => "120x90>", :medium => "500x375>" }, 
     :default_url => "/images/projects/missing.png",
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
